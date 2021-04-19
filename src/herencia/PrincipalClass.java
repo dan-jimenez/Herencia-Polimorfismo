f74676ef551102ede7e686605113ba5001d6475e
Danny Jimenez <60985321+dan-jimenez@users.noreply.github.com>
2021-04-19 16:33:32 -0600

    Menu y Principal
    .Json del menu y el archivo principal de interfaz grafica

---
+++ src/herencia/PrincipalClass.java
@@ -0,0 +1,32 @@
+
+package herencia;
+
+/**
+ *
+ * @author Danny Jimenez
+ */
+public class PrincipalClass {
+    
+    
+    public static Inicio principalWindow = new Inicio();
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Windows".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException | InstantiationException | IllegalAccessException | javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(Inicio.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+        
+        
+        principalWindow.setExtendedState(principalWindow.MAXIMIZED_BOTH);
+        principalWindow.setVisible(true);
+    }
+    
+}
